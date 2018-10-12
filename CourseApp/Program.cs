a736f135ea2bcb27e2ecfcf22c6c3b90886c49fb
User of DK12 <DK12@it.isuct.ru>
2018-10-12 14:56:49 +0400

    Second lab

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 
-namespace CourseApp
+namespace ConsoleApp3
 {
     class Program
+
     {
-        static void Main(string[] args)
+
+        public static double y(double x, double a, double b)
         {
-            Console.WriteLine("Viktorija_Mozekina!");
+            return ((Math.Log10(x * x - 1) / Math.Log((a * x * x - b), 5)));
         }
+
+
+        static void Main()
+        {
+            double a = 1.1;
+            double b = 0.09;
+
+            double[] x1 = new double[5] { 1.21, 1.76, 2.53, 3.48, 4.52 };
+
+            Console.WriteLine("Задача A:");
+
+            for (double x = 1.2; x <= 2.2; x = x + 0.2)
+            {
+                Console.WriteLine($"На шаге {x} функция y = {Math.Round(y(x,a,b), 4)}");
+            }
+
+            Console.WriteLine("Задача B:");
+
+            foreach (double i in x1)
+
+            {
+                Console.WriteLine($"Для x = {i}\t y = {y(i, a, b)}");
+            }
+            Console.ReadKey();
+        }
+
     }
 }
